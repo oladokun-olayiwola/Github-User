d0659079ef6dcfbae6790c5b8be84e20106e976b
oladokun-olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2022-07-21 15:36:37 +0100

    Done

---
+++ src/context/mockData.js/mockUser.js
@@ -0,0 +1,39 @@
+export default {
+  login: "oladokun-olayiwola",
+  id: 52329972,
+  node_id: "MDQ6VXNlcjUyMzI5OTcy",
+  avatar_url: "https://avatars.githubusercontent.com/u/52329972?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/oladokun-olayiwola",
+  html_url: "https://github.com/oladokun-olayiwola",
+  followers_url: "https://api.github.com/users/oladokun-olayiwola/followers",
+  following_url:
+    "https://api.github.com/users/oladokun-olayiwola/following{/other_user}",
+  gists_url: "https://api.github.com/users/oladokun-olayiwola/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/oladokun-olayiwola/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/oladokun-olayiwola/subscriptions",
+  organizations_url: "https://api.github.com/users/oladokun-olayiwola/orgs",
+  repos_url: "https://api.github.com/users/oladokun-olayiwola/repos",
+  events_url:
+    "https://api.github.com/users/oladokun-olayiwola/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/oladokun-olayiwola/received_events",
+  type: "User",
+  site_admin: false,
+  name: "Olayiwola",
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: "oladokunolayiw2",
+  public_repos: 35,
+  public_gists: 0,
+  followers: 1,
+  following: 1,
+  created_at: "2019-06-28T17:40:48Z",
+  updated_at: "2022-07-19T15:18:13Z",
+};
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
+// dev-yg4to51m.us.auth0.com
+
+
+//ADLbs5sizmECnyncS1Giekhaj9Dz74rp
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain='dev-pu8wyk-g.us.auth0.com'
+      clientId='GYsVn60CkUU6fa4yiwpRrT1KVL5KuzPh'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
